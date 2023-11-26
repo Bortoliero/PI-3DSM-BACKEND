0d5d561539283aa7ad21e000c4e0ffeebbceee59
Gabriel Abramovick <gabortoliero@gmail.com>
2023-11-25 21:02:38 -0300

    configuração de cors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ require('dotenv').config()
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Funciona')
+})
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
